36c6923cbd6defa21bda608fc8a578633ade8ad5
Hugo Torres Tenreiro <hugotorrestenreiro@gmail.com>
2026-09-25 06:43:42 +0200

    Update cpu struct architecture desc

--- dmg.h
+++ dmg.h
@@ -5,7 +5,7 @@
 
 typedef struct cpu
 {
-    uint16_t AF ;  // Accumulator & FLags (High: A, Low: Flags)
+    uint16_t AF ;  // Accumulator & FLags (High: A, Low: Flags (bit7:z, bit6:n, bit5:h, bit4:c))
     uint16_t BC ; // BC (High: B, Low: C)
     uint16_t DE ; // DE (High: D, Low: E)
     uint16_t HL ; // HL (High: H, Low: L)
